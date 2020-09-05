e491a6cc07241a686b9fc82849dba70449599934
jonpbak <jonpbak@vt.edu>
2020-09-04 21:13:34 -0400

    Improved row ordering for displaying board

--- hwlogic/placement.py
+++ hwlogic/placement.py
@@ -39,17 +39,11 @@ def systemSort(systems):
             homes[sys.home] = sys
         else:
             rows[sys.markers[0].size-1].append(sys)
+    p = optPermute(homes)
+    rows = [rows[p[i]] for i in range(3)]
+
     # Delete empty rows
     rows = [r for r in rows if len(r)>0]
-
-    # Rearrange rows to emphasize connectivity
-    # TODO Make it good for non-large universes
-    for i in range(len(rows)):
-        if homes[0] is not None and rows[i][0].connectsTo(homes[0]):
-            rows[i],rows[0] = rows[0],rows[i]
-    for i in range(len(rows)):
-        if homes[1] is not None and rows[i][0].connectsTo(homes[1]):
-            rows[i],rows[-1] = rows[-1],rows[i]
 
     # Rearrange systems within rows to emphasize player control
     for row in rows:
@@ -62,23 +56,90 @@ def systemSort(systems):
 
     return rows
 
-'''
-There are many ways that homeworlds can share neighboring systems.
-The shared neighbors will determine the most pleasing way to arrange the rows of same-size systems.
-Say a,b,c are the sizes of systems
-(A destroyed home is connected to all three)
+def optPermute(homes):
+    # Returns a permutation for the (possibly empty) rows to emphasize connectivity to homes
+    # The returned permutation uses 0,1,2 (not 1,2,3)
+    homeConnects = [set((1,2,3)),set((1,2,3))]
+    for i in range(2):
+        if homes[i] is None:
+            continue
+        for m in homes[i].markers:
+            homeConnects[i].remove(m.size)
+    # Number of sizes to which each home connects
+    nHC = [len(x) for x in homeConnects]
 
-Connections   | Preferred 
-0     | 1     | display order
-------+-------+-------------
-a     | b     | 0 a c b 1
-a     | a     | 0 b a b 1
-a     | a b   | 0 a c b 1
-a     | b c   | 0 a b c 1
-a     | a b c | 0 a b c 1
-a b   | a c   | 0 b a c 1
-a b   | a b   | 0 a c b 1
-a b   | a b c | 0 a b c 1
-a b c | a b c | 0 a b c 1
+    # Make it so home 0 connects to no more stars than home 1
+    if nHC[0] > nHC[1]:
+        return optPermute(homes[::-1])[::-1]
 
-'''
+    if nHC[0] == 1:
+        s0 = next(iter(homeConnects[0]))
+        if nHC[1] == 1:
+            # Both systems connect to just one size
+            s1 = next(iter(homeConnects[1]))
+            if s0 == s1:
+                # They connect to the same size
+                # Put that size in the middle, the others don't really matter
+                return [(s0-2)%3,s0-1,s0%3]
+            # Systems connect to different sizes
+            # The sum of all three sizes is 6, so the last one is 6-s0-s1
+            s2 = 6-s0-s1
+            return [s0-1,s2-1,s1-1]
+        # The largest size connected to home 1 that does not connect to home 0
+        s1 = max(homeConnects[1] - homeConnects[0])
+        # The other size
+        s2 = 6-s0-s1
+        return [s0-1,s2-1,s1-1]
+
+    # Both homes connect to at least two sizes
+    i = homeConnects[1]&homeConnects[0]
+    if len(i) == 1:
+        # The homes connect to two systems each with one in common
+        s0 = next(iter(homeConnects[0]-i))
+        s2 = next(iter(i))
+        s1 = 6-s0-s2
+        return [s0-1,s2-1,s1-1]
+
+    if nHC[1] == 2:
+        # Both homes connect to the same two sizes
+        s0 = min(homeConnects[0])
+        s1 = max(homeConnects[0])
+        s2 = 6-s0-s1
+        return [s0-1,s2-1,s1-1]
+
+    # At least one home is destroyed
+    if nHC[0] == 3:
+        # Both homes are destroyed
+        return [0,1,2]
+    # home 1 is destroyed, home 0 is connected to two sizes
+    d = homeConnects[1]-homeConnects[0]
+    s1 = next(iter(d))
+    s0 = min(homeConnects[0])
+    s2 = 6-s0-s1
+    return [s0-1,s2-1,s1-1]
+
+if __name__=='__main__':
+    class Spam:
+        def __init__(self,size):
+            self.size = size
+    class Foo:
+        def __init__(self,sizes):
+            self.markers = [Spam(x) for x in sizes]
+        def __str__(self):
+            return '('+','.join([str(x.size) for x in self.markers])+')'
+
+    import sys
+    sys.path.append('/home/jonathan/whome/GoogleDrive/ProjectEuler/lib/')
+    from powerset import powerset
+    for m0 in powerset(range(1,4),b=2):
+        h0 = Foo(m0)
+        for m1 in powerset(range(1,4),b=2):
+            h1 = Foo(m1)
+            p = optPermute([h0,h1])
+            print(h0,[x+1 for x in p],h1)
+
+
+
+
+
+
