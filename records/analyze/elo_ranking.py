a12c666047e45f497d1381f16fc1a0da27e5c486
jonpbak <jonpbak@vt.edu>
2023-10-05 17:10:11 -0600

    Separated functions, preparing for MLE ELO

---
+++ records/analyze/elo_ranking.py
@@ -0,0 +1,9 @@
+'''
+Read a bunch of BGA records, track pairwise results, and compute ML Elo scores
+If the Elo scores of two players are A and B,
+the probability of A beating B is
+1/(1 + 10^((B-A)/400) )
+'''
+
+
+
--- records/analyze/read_results.py
+++ records/analyze/read_results.py
@@ -1,30 +1,19 @@
-'''
-Read a bunch of BGA records, track pairwise results,
-and compute pagerank score
-'''
-
-# Read all the files to build matrix?
-# Otherwise, just read last result
-runit = 1
-# Include BGA player ID in printout?
-rank_bga_id = 0
-# Number of player ranks to reveal
-n_to_show = 100
-# Use Discord spoiler tags?
-spoiler_tag = 0
+'Get HW results from BGA files or from pickled results'
 
 import pickle
 import os
-from scipy.sparse import coo_array,csc_array
-from scipy.sparse.linalg import eigs
+from scipy.sparse import coo_array
 import numpy as np
 from bga_basic_read import read_players,opening_results
-import networkx as nx
-from counter import Counter
 
 root_dir = '/mnt/c/Users/Bakers/Documents/hw_replays/'
 
-if runit:
+def results_array(runit):
+    if runit:
+        return file_array()
+    return pickle_array()
+
+def file_array():
     ######################################
     # Create win matrix by reading files #
     ######################################
@@ -99,7 +88,6 @@ if runit:
                 break
 
     n = len(lookup_index)
-    print('Total number of players:',n)
 
     # Put a zero in the corner to make matrix square
     results.append(0)
@@ -111,82 +99,15 @@ if runit:
 
     with open('results_mat.pkl','bw') as fout:
         pickle.dump( (A,lookup_id,lookup_index,lookup_name) ,fout )
-else:
+
+    return (A,lookup_id,lookup_index,lookup_name)
+
+def pickle_array():
     ################################################
     # Create win matrix by loading previous result #
     ################################################
     print('Using previously saved results')
     with open('results_mat.pkl','br') as fin:
         (A,lookup_id,lookup_index,lookup_name) = pickle.load( fin )
-    n = len(lookup_index)
-    print('Total number of players:',n)
-
-######################################
-# Find strongly connected components #
-# and identify the largest one       #
-######################################
-G = nx.DiGraph()
-
-for i,j in zip(*A.nonzero()):
-    G.add_edge(i,j)
-
-print('Identifying strongly connected components of player victory graph')
-comps = list(nx.strongly_connected_components(G))
-C = Counter()
-
-big_comp = comps[0]
-for comp in comps:
-    C.add(len(comp))
-    if len(comp) > len(big_comp):
-        big_comp = comp
-n_big = len(big_comp)
-
-x = list(C.items())
-x.sort()
-for size,count in x:
-    print('  {} components of size {}'.format(count,size))
-print('Focusing analysis on a component of size',n_big)
-print()
-
-# Prepare to translate between original indexing and big_comp submatrix
-big_comp = list(big_comp)
-lookup_sub = dict(zip(big_comp,range(n_big)))
-
-B = A[big_comp][:,big_comp]
-
-print('Computing PageRank')
-B /= B.sum(0)
-w,v = eigs(B)
-
-i = np.argmax(w)
-whi = w[i]
-vhi = v.T[i]
-if vhi.min() < 0:
-    vhi *= -1
-
-if not np.all(vhi>=0):
-    raise Exception('Eigenvector is not non-negative. Something is very wrong.')
-
-# Sort players by their eigenvector entry
-p = np.argsort(vhi)
-for place,k in enumerate(p[:-n_to_show-1:-1]):
-    orig_index = big_comp[k]
-    kid = lookup_id[orig_index]
-    if rank_bga_id:
-        print('{}. {}: {}'.format(
-            place+1,
-            kid,
-            '/'.join(lookup_name[kid])
-        ))
-    else:
-        if spoiler_tag:
-            print('{}. ||{}||'.format(
-                place+1,
-                '/'.join(lookup_name[kid])
-            ))
-        else:
-            print('{}. {}'.format(
-                place+1,
-                '/'.join(lookup_name[kid])
-            ))
+    return (A,lookup_id,lookup_index,lookup_name)
 
